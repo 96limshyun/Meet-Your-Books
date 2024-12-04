6c761a9172688a1b1cc00ee27d30838df02fc3fa
임승현 <96limshyun@gmail.com>
2024-12-04 14:06:35 +0900

    feat: 백엔드 auth 인증 구현
    - express 구축
    - /auth 라우터 구축
    - 프론트에서 보낸 인가코드를 kakao로 보내 엑세스토큰 발급
    - kakao에서 받은 엑세스토큰으로 유저 정보 다시 요청
    - 받은 유저정보를 프론트로 전송

---
+++ BE/constants/index.ts
@@ -0,0 +1,5 @@
+export const MESSAGE = {
+    AUTH_SUCCESSFUL: "로그인 성공",
+    EMPTY_TOKEN: "토큰을 받아오는데 실패했습니다.",
+    AUTH_FAILED: "로그인에 실패했습니다."
+}
--- FE/src/pages/Login.tsx
+++ FE/src/pages/Login.tsx
@@ -1,18 +1,30 @@
 import { useEffect } from "react";
 
+const Rest_api_key = import.meta.env.VITE_KAKAO_REST_API_KEY;
+const redirect_uri = import.meta.env.VITE_AUTH_REDIRECT_URL;
+const kakaoURL = `${import.meta.env.VITE_KAKAO_URL}client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
+
 const Login = () => {
-    const Rest_api_key = import.meta.env.VITE_KAKAO_REST_API_KEY;
-    const redirect_uri = import.meta.env.VITE_AUTH_REDIRECT_URL;
-    const kakaoURL = `${import.meta.env.VITE_KAKAO_URL}client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
-    const handleLogin = () => {
-        window.location.href = kakaoURL;
-    };
+    const handleLogin = () => window.location.href = kakaoURL;
 
     useEffect(() => {
+        const authFetch = async(code: string) => {
+            const response = await fetch("http://localhost:4000/kakaoAuth", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ code }),
+            })
+            const data = await response.json()
+            console.log(data)
+        }
         const code = new URL(window.location.href).searchParams.get("code");
         if(code) {
-            console.log(code)
+            authFetch(code)
         }
+
+
     }, [])
     return (
         <>
