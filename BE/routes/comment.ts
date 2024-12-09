8baf4caeda30a4084c33bd9feeeca11fddd7f481
임승현 <96limshyun@gmail.com>
2024-12-09 21:22:55 +0900

    feat:
    fix: 해당 책에 댓글이 없을때 빈배열을 리턴하도록 수정
    
    #15

--- BE/routes/comment.ts
+++ BE/routes/comment.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, {Request, Response, Router} from "express";
 import CommentGroup from "../Models/CommentSchema";
 
 const commentRouter = express.Router();
@@ -7,8 +7,11 @@ commentRouter.get("/comments/:isbn", async (req, res) => {
     try {
         const { isbn } = req.params;
         const commentGroup = await CommentGroup.findById(isbn);
-
-        res.status(200).json(commentGroup?.comments);
+        if (!commentGroup) {
+            res.status(200).json([]);
+        } else {
+            res.status(200).json(commentGroup?.comments);
+        }
     } catch (error) {
         res.status(500).json({ message: "댓글 조회 중 에러 발생" });
     }
@@ -17,9 +20,10 @@ commentRouter.get("/comments/:isbn", async (req, res) => {
 commentRouter.post("/comments/:isbn", async (req, res) => {
     try {
         const { isbn } = req.params;
-        const { username, content } = req.body;
+        const { userid, username, content } = req.body;
 
         const newComment = {
+            userid,
             username,
             content,
             timestamp: new Date(),
@@ -32,9 +36,9 @@ commentRouter.post("/comments/:isbn", async (req, res) => {
         } else {
             await CommentGroup.create({ _id: isbn, comments: [newComment] });
         }
-        res.status(201).json({message: "댓글 추가 성공"})
+        res.status(201).json({ message: "댓글 추가 성공" });
     } catch (error) {
-        res.status(400).json({message: "댓글 추가 실패"})
+        res.status(400).json({ message: "댓글 추가 실패" });
     }
 });
 
