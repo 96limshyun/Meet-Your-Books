5b40db1a80fc78c2fa737ab40a61808dc4a613c5
임승현 <96limshyun@gmail.com>
2024-12-09 17:14:13 +0900

    style: 책 상제 정보 요청 및 정보 스타일링 구현
    - 메인 페이지의 책 클릭 시 책 상세 페이지로 이동
    - isbn 코드로 상세 정보로 데이터 요청 기능 구현
    - 상제 정보 스타일링
    
    #15

--- FE/src/pages/BookDetail.tsx
+++ FE/src/pages/BookDetail.tsx
@@ -1,6 +1,38 @@
+import { BookDetailCard } from "@components/Detail";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+import useGetQuery from "@/hooks/Queries/useGetQuery";
+import { BookDetailType } from "@/types/bookDetailType";
 
 const BookDetail = () => {
-    return <div>BookDetail</div>;
+    const { bookId } = useParams();
+    const { data, isLoading } = useGetQuery(
+        "usageAnalysisList",
+        `${bookId}`,
+        `&isbn13=${bookId}`
+    );
+    if (isLoading) return <div>...loading</div>;
+
+    const book: BookDetailType = data.response.book;
+    return (
+        <Container>
+            <BookDetailCard bookData={book}/>
+        </Container>
+    );
 };
 
 export default BookDetail;
+
+const Container = styled.section`
+    max-width: 1044px;
+    padding: 0 20px;
+    height: 100%;
+    margin: 0px auto;
+`;
+
+
+// 책 상세 정보
+// 대출 추이 그래프
+// 소장도서관 보기(팝업으로?)
+// 댓글
