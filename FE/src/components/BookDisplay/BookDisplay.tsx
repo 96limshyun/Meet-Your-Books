64ac2b00c9aa3d83f4d3138a5ca2a60993aab539
임승현 <96limshyun@gmail.com>
2024-12-19 16:21:17 +0900

    docs: 주석 삭제

--- FE/src/components/BookDisplay/BookDisplay.tsx
+++ FE/src/components/BookDisplay/BookDisplay.tsx
@@ -24,7 +24,6 @@ const BookDisplay = () => {
     if (isLoading) {
         return (
             <BookContainer>
-                {/* <LoadingSpin /> */}
                 <LoadingBook/>
             </BookContainer>
         );
