fae3d59a398c7f68fa2443b22ba87dfa8271abab
임승현 <96limshyun@gmail.com>
2024-12-28 17:02:04 +0900

    fix: 로딩중 마우스가 카드를 벗어나면 로딩이 전체 화면으로 벗어나는 버그 수정

--- FE/src/components/Common/FavoriteBtn/FavoriteBtn.tsx
+++ FE/src/components/Common/FavoriteBtn/FavoriteBtn.tsx
@@ -1,5 +1,5 @@
 import { StarOutlined, StarFilled } from "@ant-design/icons";
-import { message } from "antd";
+import { message, Spin } from "antd";
 import React from "react";
 import styled from "styled-components";
 
@@ -7,8 +7,6 @@ import useAddFavoriteMutation from "@/hooks/Queries/favorites/useAddFavoriteMuta
 import { useFavoritesQuery } from "@/hooks/Queries/favorites/useFavoritesQuery";
 import useRemoveFavoriteMutation from "@/hooks/Queries/favorites/useRemoveFavoriteMutation";
 import { BookDoc } from "@/types/booksType";
-
-import LoadingBook from "../LoadingBook/LoadingBook";
 
 interface FavoritesBtnProps {
     item: BookDoc;
@@ -25,13 +23,14 @@ const FavoriteBtn = ({ item }: FavoritesBtnProps) => {
         (book: { isbn13: string }) => book.isbn13 === item.isbn13
     );
 
-    const isPending = addFavoriteMutation.isPending || removeFavoriteMutation.isPending;
+    const isPending =
+        addFavoriteMutation.isPending || removeFavoriteMutation.isPending;
 
     const handleFavorite = (e: React.MouseEvent) => {
         e.stopPropagation();
-        if (!USER_INFO?.id) return message.error("로그인이 필요합니다.")
+        if (!USER_INFO?.id) return message.error("로그인이 필요합니다.");
         if (isPending) return;
-        
+
         if (isFavorite) {
             removeFavoriteMutation.mutate(item.isbn13);
         } else {
@@ -41,13 +40,12 @@ const FavoriteBtn = ({ item }: FavoritesBtnProps) => {
 
     return (
         <>
-            <ButtonWrap onClick={handleFavorite}>
-                {isFavorite ? <FavoriteButton /> : <UnFavoriteButton />}
-            </ButtonWrap>
-            {isPending && (
-                <Overlay>
-                    <LoadingBook />
-                </Overlay>
+            {isPending ? (
+                <Spin />
+            ) : (
+                <ButtonWrap onClick={handleFavorite}>
+                    {isFavorite ? <FavoriteButton /> : <UnFavoriteButton />}
+                </ButtonWrap>
             )}
         </>
     );
@@ -64,16 +62,4 @@ const UnFavoriteButton = styled(StarOutlined)`
 `;
 const FavoriteButton = styled(StarFilled)`
     color: #fadb14;
-`;
-
-const Overlay = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: rgba(255, 255, 255, 0.7);
 `;
