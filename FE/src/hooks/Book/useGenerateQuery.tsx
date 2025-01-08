6ec43e0da4ca0e9fcb93424af5959853d30b48d5
임승현 <126482821+96limshyun@users.noreply.github.com>
2025-01-08 18:14:41 +0900

    Revert "refactor: 검색어 및 필터 상태 URL 동기화 구현"

--- FE/src/hooks/Book/useGenerateQuery.tsx
+++ FE/src/hooks/Book/useGenerateQuery.tsx
@@ -1,59 +1,26 @@
-import { useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useMemo } from "react";
 
-import { DROP_DOWN_ITEMS } from "@/constants";
 import useBookStore from "@/stores/bookStore";
 
 const useGenerateQuery = () => {
-    const {
-        searchText,
-        size,
-        sort,
-        order,
-        selectedValue,
-        keyword,
-        setSearchText,
-        setOrder,
-        setSort,
-        setSelectedValue,
-        setKeyword,
-    } = useBookStore();
-    const [searchParams, setSearchParams] = useSearchParams();
+    const { searchText, size, sort, order, selectedValue, keyword} =
+        useBookStore();
 
-    useEffect(() => {
+    const queryString = useMemo(() => {
         const queryParams = {
-            ...(searchText && {
-                [selectedValue.value]: searchText.replace(/\s+/g, ""),
-            }),
-            pageSize: size.toString(),
+            ...(searchText && { [selectedValue.value]: searchText.replace(/\s+/g, "") }),
+            pageSize: size,
             ...(sort && { sort }),
             ...(order && { order }),
             ...(keyword.length > 0 && { keyword: keyword.join(";") }),
-            exactMatch: "false",
+            exactMatch: false,
         };
-        setSearchParams(queryParams);
-    }, [
-        searchText,
-        size,
-        sort,
-        order,
-        selectedValue,
-        keyword,
-        setSearchParams,
-    ]);
-
-    useEffect(() => {
-        const params = Object.fromEntries(searchParams.entries())
-        const searchKeys = ["title", "author", "publisher"].find(key => params[key]);
-        const selectedDropDownItem = DROP_DOWN_ITEMS.find(item => item.value === searchKeys)
-        const keywords = params.keyword ? params.keyword.split(";") : []
-
-        setSearchText(searchKeys ? params[searchKeys] : "")
-        setSort(params.sort || "")
-        setOrder(params.order || "")
-        setKeyword(keywords)
-        setSelectedValue(selectedDropDownItem || { value: "title", label: "제목" })
-    }, [searchParams, setKeyword, setOrder, setSearchText, setSelectedValue, setSort])
+    
+        return Object.entries(queryParams)
+            .map(([key, value]) => `${key}=${value}`)
+            .join("&");
+    }, [searchText, selectedValue.value, size, sort, order, keyword]);
+    return queryString;
 };
 
 export default useGenerateQuery;
