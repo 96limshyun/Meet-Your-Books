bd9672ae4335e237c4e759880fac6596871d424b
임승현 <96limshyun@gmail.com>
2025-06-13 13:30:19 +0900

    refactor: Button 컴포넌트를 twc + cva 기반으로 마이그레이션
    * 기존 styled-components 기반 Button 제거
    * class-variance-authority(cva)로 variant 관리
    * twMerge + twc 조합으로 유연한 스타일 적용 방식 구현
    
    #77

--- FE/src/components/Common/index.ts
+++ FE/src/components/Common/index.ts
@@ -1,5 +1,4 @@
 export { default as Button } from "./Button/Button";
-export { default as Image } from "./Image/Image";
 export { default as Spacing } from "./Spacing/Spacing";
 export { default as FavoriteBtn } from "./FavoriteBtn/FavoriteBtn";
 export { default as Overlay } from "./Overlay/Overlay";
